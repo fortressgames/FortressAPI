9696ca6767bc954cf8a6bd75b317acf43ac926b9
FortressGames <fortressgames1@gmail.com>
2022-12-25 02:12:59 +0000

    added npc

--- plugin/src/main/java/net/fortressgames/fortressapi/listeners/ClickEntityListener.java
+++ plugin/src/main/java/net/fortressgames/fortressapi/listeners/ClickEntityListener.java
@@ -13,6 +13,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
@@ -22,10 +24,18 @@ public class ClickEntityListener extends PacketAdapter implements Listener {
 		super(FortressAPI.getInstance(), PacketType.Play.Client.USE_ENTITY);
 	}
 
+	private final List<Player> delay = new ArrayList<>();
+
 	@SneakyThrows
 	@Override
 	public void onPacketReceiving(PacketEvent e) {
 		PacketContainer packet = e.getPacket();
+
+		if(delay.contains(e.getPlayer())) return;
+		delay.add(e.getPlayer());
+		Bukkit.getScheduler().runTask(FortressAPI.getInstance(), () -> {
+			delay.remove(e.getPlayer());
+		});
 
 		for(Map.Entry<Entity, BiConsumer<Player, Entity>> map : FortressAPI.click.entrySet()) {
 
@@ -39,9 +49,8 @@ public class ClickEntityListener extends PacketAdapter implements Listener {
 				try {
 					objectValueB.getClass().getDeclaredField("a");
 					//LEFT CLICK
-					Bukkit.getScheduler().runTask(FortressAPI.getInstance(), () -> {
-						FortressAPI.click.get(map.getKey()).accept(e.getPlayer(), map.getKey());
-					});
+
+					Bukkit.getScheduler().runTask(FortressAPI.getInstance(), () -> FortressAPI.click.get(map.getKey()).accept(e.getPlayer(), map.getKey()));
 
 				} catch (NoSuchFieldException ee) {
 					//RIGHT CLICK
